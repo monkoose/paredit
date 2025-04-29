cb6b4cb4cabbd92035974a8084b18656d8a5d5d4
monkoose <pythonproof@gmail.com>
2025-04-29 11:45:51 +0300

    Refactor

--- lua/paredit/init.lua
+++ lua/paredit/init.lua
@@ -3,6 +3,9 @@ local M = {}
 local nvim_buf_get_lines = vim.api.nvim_buf_get_lines
 local nvim_win_get_cursor = vim.api.nvim_win_get_cursor
 local nvim_win_set_cursor = vim.api.nvim_win_set_cursor
+-- local nvim_feedkeys = vim.api.nvim_feedkeys
+
+local cmd = vim.cmd
 
 ---@param linenr integer
 ---@return integer
@@ -12,6 +15,10 @@ local function line_indent_size(linenr)
    return size ---@diagnostic disable-line: return-type-mismatch
 end
 
+-- local function feedkeys(keys, escape_ks)
+--    nvim_feedkeys(keys, "xt", escape_ks or false)
+-- end
+
 ---Reindent form and adjust cursor position
 ---If `linenr` is present, then additionally reindent to this line
 ---@param linenr? integer
@@ -19,8 +26,8 @@ function M.reindent_form(linenr)
    local pos = nvim_win_get_cursor(0)
    local old_indent = line_indent_size(pos[1])
 
-   vim.cmd("normal! =ib")
-   if linenr then vim.cmd("normal! =" .. linenr .. "G") end
+   cmd.normal({ "=ib", bang = true })
+   if linenr then cmd.normal({ "=" .. linenr .. "G", bang = true }) end
 
    local new_indent = line_indent_size(pos[1])
    pos[2] = pos[2] + new_indent - old_indent
