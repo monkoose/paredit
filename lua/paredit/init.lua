84dc8d0b40db8dba02955fe351f72e5d2405bddb
monkoose <pythonproof@gmail.com>
2025-04-29 11:22:08 +0300

    Update

--- lua/paredit/init.lua
+++ lua/paredit/init.lua
@@ -9,17 +9,19 @@ local nvim_win_set_cursor = vim.api.nvim_win_set_cursor
 local function line_indent_size(linenr)
    local linestr = nvim_buf_get_lines(0, linenr - 1, linenr, false)[1]
    local _, size = string.find(linestr, "^%s*")
-   return size
+   return size ---@diagnostic disable-line: return-type-mismatch
 end
 
+---Reindent form and adjust cursor position
+---If `linenr` is present, then additionally reindent to this line
+---@param linenr? integer
 function M.reindent_form(linenr)
    local pos = nvim_win_get_cursor(0)
    local old_indent = line_indent_size(pos[1])
-   if linenr then
-      vim.cmd("normal! =" .. (linenr - pos[1]) .. "j")
-   else
-      vim.cmd("normal! =ib")
-   end
+
+   vim.cmd("normal! =ib")
+   if linenr then vim.cmd("normal! =" .. linenr .. "G") end
+
    local new_indent = line_indent_size(pos[1])
    pos[2] = pos[2] + new_indent - old_indent
    nvim_win_set_cursor(0, pos)
